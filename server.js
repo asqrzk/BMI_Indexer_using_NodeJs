eb680b4be4c03a21c76879b95e5c2d8e9b7bd67f
asqrzk <ashique.kainote@gmail.com>
2021-11-21 00:29:48 +0530

    fixed port issue

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require("./index");
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log("We're live on port 4k");
+    console.log("We're live on port "+port);
 });
